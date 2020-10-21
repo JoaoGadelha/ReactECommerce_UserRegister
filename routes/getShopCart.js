2e3715faa3effbd66715f7941f4a2b807ed970f1
Joao <joaoricardotg@gmail.com>
2020-10-21 12:05:07 -0300

    Implementing backend for shopping cart

--- routes/getShopCart.js
+++ routes/getShopCart.js
@@ -5,6 +5,7 @@ let User = require('../usrSchema.js');
 getShopCart.post('/:id', async (req, res) => {
     try {
         let shopCart = await User.find({ _id: req.params.id });
+        console.log(shopCart);
         return res.json({shopCart:shopCart[0].shopCart});
     } catch (err) {
         res.json({ message: err });
--- routes/setShopCart.js
+++ routes/setShopCart.js
@@ -4,8 +4,12 @@ let User = require('../usrSchema.js');
 
 setShopCart.post('/', async (req, res) => {
     try {
-        console.log(req.body.quantCart);
-        return '';
+        console.log(req.body.id, req.body.quantity, req.body.userId);
+        let modify = await User.updateOne({ _id: req.body.userId, "shopCart.id":req.body.id }, { $set: { 'shopCart.$.quantity': req.body.quantity } });
+       // console.log('modify :' + modify.ok + ',' + modify.n + ',' + modify.nModified);
+        let modifiedCart = await User.find({ _id: req.body.userId  });
+        //console.log('modified: ' + modifiedCart);
+        return res.json(modifiedCart[0].shopCart);
     } catch (err) {
         res.json({ message: err });
     }
