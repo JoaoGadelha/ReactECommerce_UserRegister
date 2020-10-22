405a8577c76d0d0cfb5b4981fa161c233626d480
Joao <joaoricardotg@gmail.com>
2020-10-22 18:47:06 -0300

    Updating signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -6,12 +6,22 @@ signup.post('/', async (req, res) => {
     if (req.body.password !== req.body.password2) {
         return res.send({ message: 'different passwords' })
     }
-    usr = new User({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-        shopCart: []
-    });
+    if(req.body.saveProduct !== '') {
+        usr = new User({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+            shopCart: [req.body.saveProduct]
+        });
+    } else {
+        usr = new User({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+            shopCart: []
+        });
+    }
+    
     try {
         let newUsr = await usr.save();
         return res.json(newUsr);
