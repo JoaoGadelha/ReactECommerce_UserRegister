8510e2937c99d5d7ab6e198f1daf6193d6169a95
Joao <joaoricardotg@gmail.com>
2020-10-27 23:14:06 -0300

    Updating setShopCart

--- routes/setShopCart.js
+++ routes/setShopCart.js
@@ -4,15 +4,18 @@ let User = require('../usrSchema.js');
 
 setShopCart.post('/', async (req, res) => {
     try {
-        console.log(req.body.id, req.body.quantity, req.body.userId);
+        //console.log(req.body.id, req.body.quantity, req.body.userId);
         if (req.body.quantity <= 0) { // if a product should be removed from the cart
             await User.updateOne({ _id: req.body.userId }, { $pull: { 'shopCart': { 'id': req.body.id } } });
-        } else {//check if wether the product is already present in the cart or not
-            let productIsInCart = await User.find({ _id: req.body.userId, "shopCart.id": req.body.id });
-            if (productIsInCart.length === 0) {
-                await User.updateOne({ _id: req.body.userId }, { $push: { shopCart: { quantity: req.body.quantity, id: req.body.id } } });
-            } else {
-                await User.updateOne({ _id: req.body.userId, "shopCart.id": req.body.id }, { $set: { 'shopCart.$.quantity': req.body.quantity } });
+        } else { //check if a product must be inserted or not
+            if (Object.keys(req.body.id).length > 0) {
+                //check if wether the product is already present in the cart or not
+                let productIsInCart = await User.find({ _id: req.body.userId, "shopCart.id": req.body.id });
+                if (productIsInCart.length === 0) {
+                    await User.updateOne({ _id: req.body.userId }, { $push: { shopCart: { quantity: req.body.quantity, id: req.body.id } } });
+                } else {
+                    await User.updateOne({ _id: req.body.userId, "shopCart.id": req.body.id }, { $set: { 'shopCart.$.quantity': req.body.quantity } });
+                }
             }
         }
         // console.log('modify :' + modify.ok + ',' + modify.n + ',' + modify.nModified);
